447addd3f3f8c78d1b2a194271e7fe835b3e9f95
Richard Felintro Da Silva <47417895867@info.local>
2024-08-07 17:22:20 -0300

    Exercício adicionado.

---
+++ SPRINT 3 - Preparatório Processo Seletivo/2024-08-07 Exercício de lógica de programação/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Informe a nota do aluno");
+switch (float.Parse(Console.ReadLine()!)){
+    case >= 7:
+    Console.WriteLine("Aluno aprovado.");
+    break;
+        case >= 5:
+        Console.WriteLine("Aluno em recuperação.");
+    break;
+        default:
+        Console.WriteLine("Aluno reprovado.");
+    break;
+}
