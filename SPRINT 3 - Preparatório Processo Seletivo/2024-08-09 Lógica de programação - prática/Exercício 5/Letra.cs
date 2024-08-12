3108f790a4cb51f22caa58509a45f7bc6e1a98bc
Richard Felintro <richardfelintro06@gmail.com>
2024-08-12 15:59:54 -0300

    Exercício 5 optimizado.

--- SPRINT 3 - Preparatório Processo Seletivo/2024-08-09 Lógica de programação - prática/Exercício 5/Letra.cs
+++ SPRINT 3 - Preparatório Processo Seletivo/2024-08-09 Lógica de programação - prática/Exercício 5/Letra.cs
@@ -8,6 +8,6 @@ namespace Exercício_5
     public class Letra
     {
         public char letra { get; set; }
-        public int quantidade { get; set; }
+        public int quantidade = 0;
     }
 }
